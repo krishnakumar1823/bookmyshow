91e7ff0468d5b36645576be1db0283b3e8528997
krishnakumar1823 <kumarkrishna180320@gmail.com>
2023-09-28 04:13:34 +0530

    commit

--- src/Component/Browserrouter/Router.js
+++ src/Component/Browserrouter/Router.js
@@ -8,7 +8,7 @@ import { Checkout } from "../Checkout/Checkout"
 export const Router=()=>{
 
     return(
-        <BrowserRouter basename="/test_repository">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" exact element={<Home/>}></Route>
                 <Route path="/details" element={<Details/>}></Route>
